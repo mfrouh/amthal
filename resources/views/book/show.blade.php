9e735a51dcfa41fdf8842c1c8691e3b5e46e638f
mohamed frouh <mohamedfrouh96@gmail.com>
2020-04-10 20:11:01 +0200

    first commit

--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -4,10 +4,25 @@
 @endsection
 @section('content')
 <div class="container">
-    {{-- <div class="row">
-        <x-infowriter :writer=$writer></x-infowriter>
+    <div class="row">
+        <div class="col-md-3">
+            <img src="{{url('/storage/book')}}/{{$book->image}}" alt="" class="img-book">
+        </div>
+        <div class="col-md-9">
+            <div class="card">
+              <div class="card-body">
+                   <div class="row p-2">
+                       @lang('home.name'):{{$book->name}}
+                   </div>
+                   <div class="row p-2">
+                       @lang('home.writer'):{{$book->writer->name}}
+                   </div>
+                   <div class="row p-2">
+                       @lang('home.url'):{{$book->url}}
+                   </div>
+               </div>
+           </div>
+        </div>
     </div>
-    <hr>
-        <x-writerarticles :writer=$writer></x-writerarticles> --}}
 </div>
 @endsection
